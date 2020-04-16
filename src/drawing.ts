79ca7b3bc52fe134d10fddcdcef1f65d31b08a84
Ruby Quail <ruby@rubyquail.design>
2020-04-16 15:21:56 +1000

    add faux cubic
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/drawing.ts
+++ src/drawing.ts
@@ -75,11 +75,11 @@ export function drawFauxQuadLoop(
     const toDraw = loop.slice();
     const ptsNo = toDraw.length;
 
-    if (close && !ptsNo)
+    if (close && !(ptsNo % 2))
         throw new Error(
             `in order to close a Faux quad loop, there needs to be an even number of input points. ${ptsNo} points where put in`
         );
-    if (!close && ptsNo)
+    if (!close && ptsNo % 2)
         throw new Error(
             `in order to draw a open Faux quad loop, there needs to be an odd number of input points. ${ptsNo} points where put in`
         );
@@ -91,6 +91,51 @@ export function drawFauxQuadLoop(
         const [x1, y1, x2, y2] = [...toDraw[i], ...toDraw[i + 1]];
 
         ctx.quadraticCurveTo(x1, y1, x2, y2);
+    }
+    if (!isCtx(ctx)) return ctx.toString();
+}
+
+export function drawFauxCubicLoop(
+    loop: Loop,
+    close: boolean,
+    ctx?: P.Path
+): string;
+export function drawFauxCubicLoop(
+    loop: Loop,
+    close: boolean,
+    ctx: CanvasRenderingContext2D
+): void;
+export function drawFauxCubicLoop(
+    loop: Loop,
+    close: boolean,
+    ctx: Drawable = P.path()
+) {
+    const toDraw = loop.slice();
+    const inputLength = toDraw.length;
+    const canDrawOpen = (inputLength - 1) % 3 === 0;
+    const canDrawClosed = inputLength % 3 === 0;
+
+    if (!canDrawOpen && !canDrawClosed)
+        throw new Error(`incorrect number of input points`);
+    if (close && !canDrawClosed)
+        throw new Error(
+            `incorrect number of input points, needs 2 more points`
+        );
+    if (!close && !canDrawOpen)
+        throw new Error(`incorrect number of input points, needs 1 more point`);
+
+    const start = toDraw.shift();
+
+    if (close) toDraw.push(start);
+    ctx.moveTo(...start);
+    for (let i = 0; i < toDraw.length; i++) {
+        const [x1, y1, x2, y2, x3, y3] = [
+            ...toDraw[i],
+            ...toDraw[i + 1],
+            ...toDraw[i + 2],
+        ];
+
+        ctx.bezierCurveTo(x1, y1, x2, y2, x3, y3);
     }
     if (!isCtx(ctx)) return ctx.toString();
 }
