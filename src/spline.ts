d6ac6fbea4c1b75570f8f8286b099887f0e54e50
Ruby Quail <ruby@rubyquail.design>
2020-08-23 17:16:17 +1000

    change from concat to push
    Signed-off-by: Ruby Quail <ruby@rubyquail.design>

--- src/spline.ts
+++ src/spline.ts
@@ -8,25 +8,25 @@ import { range } from 'd3-array';
  * @param outputResolution - resolution of the output loop
  */
 export function* spline(
-    loop: Loop,
-    degree: number,
-    close: boolean,
-    outputResolution?: number
+  loop: Loop,
+  degree: number,
+  close: boolean,
+  outputResolution?: number
 ) {
-    const resolution = outputResolution || loop.length;
-    const len = loop.length;
-    let toDraw = loop.slice(0);
-    let knots = range(len - (degree + 1));
+  const resolution = outputResolution || loop.length;
+  const len = loop.length;
+  let toDraw = loop.slice(0);
+  let knots = range(len - (degree + 1));
 
-    knots.concat(new Array<number>(degree + 1).fill(knots.length));
-    knots.unshift(...new Array<number>(degree + 1).fill(0));
-    if (close) {
-        toDraw.concat(toDraw.slice(0, degree + 1));
-        knots = range(toDraw.length + (degree + 1));
-    }
-    for (let index = 0; index < resolution; index++) {
-        const t = index / resolution;
+  knots.push(...new Array<number>(degree + 1).fill(knots.length));
+  knots.unshift(...new Array<number>(degree + 1).fill(0));
+  if (close) {
+    toDraw.push(...toDraw.slice(0, degree + 1));
+    knots = range(toDraw.length + (degree + 1));
+  }
+  for (let index = 0; index < resolution; index++) {
+    const t = index / resolution;
 
-        yield interpolate(t, degree, toDraw, knots);
-    }
+    yield interpolate(t, degree, toDraw, knots);
+  }
 }
